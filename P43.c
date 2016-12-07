af816ad18ce5e0767fd6dbf5ac6e6d1935e0f6ee
Snickeen <ff@snickeen.com>
2016-12-07 15:27:13 +0800

    Create P43.c

---
+++ P43.c
@@ -0,0 +1,17 @@
+
+//求最大公约数 
+int gcd(int a,int b){
+	if(b==0) return a;
+	else return gcd(b,a%b);
+} 
+
+//简化上面的代码 
+int GCD(int a,int b){
+	return b==0 ? a : GCD(b,a%b);
+}
+
+
+//求最小公倍数
+int lcm(int a,int b){
+	return a/gcd(a,b)*b;
+}
